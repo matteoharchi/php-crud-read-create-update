67e04d6a68fab0b1d5040300905a20ed8bca9164
Matteo Harchi <matteo.harchi@gmail.com>
2020-09-29 17:30:05 +0200

    risolto problema errore 500

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-include __DIR__ . '/partials/home/server.php';
 include __DIR__ . '/partials/templates/header.php';
-var_dump($results);
+include __DIR__ . '/partials/home/server.php';
+
+
 ?>
 
 
     <body>
+    
+    
        <div class="container">
            <table>
                <thead>
@@ -19,17 +22,18 @@ var_dump($results);
                    </tr>
                </thead>
                <tbody>
-                <?php foreach($results as $room){?>
+                    <?php foreach ($results as $room) {?>
                    <tr>
-                       <td><?php echo $room['id']?></td>
-                       <td><?php echo $room['room_number']?></td>
-                       <td><?php echo $room['floor']?></td>
+                        <td><?php echo $room['id']?></td>
+                        <td><?php echo $room['room_number']?></td>
+                        <td><?php echo $room['floor']?></td>
                        <td> <a href="">VIEW</a></td>
                        <td> <a href="">UPDATE</a></td>
                        <td> <a href="">DELETE</a></td>
                    </tr>
+                    <?php } ?>
                </tbody>
-                <?php}?>
+
            </table>
        </div> 
     </body>
